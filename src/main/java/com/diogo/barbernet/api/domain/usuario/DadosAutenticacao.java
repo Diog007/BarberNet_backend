d70ff318f29865f8a19f94e3855a9b58ed4b221f
Diog007 <diogo.mz123@gmail.com>
2024-02-25 16:55:46 -0300

    Implemented the authentication method in the database

---
+++ src/main/java/com/diogo/barbernet/api/domain/usuario/DadosAutenticacao.java
@@ -0,0 +1,4 @@
+package com.diogo.barbernet.api.domain.usuario;
+
+public record DadosAutenticacao(String login, String senha) {
+}
--- src/main/java/com/diogo/barbernet/api/infra/security/SecurityConfigurations.java
+++ src/main/java/com/diogo/barbernet/api/infra/security/SecurityConfigurations.java
@@ -2,6 +2,8 @@ package com.diogo.barbernet.api.infra.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -15,6 +17,10 @@ public class SecurityConfigurations {
         return http.csrf(csrf -> csrf.disable())
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and().build();
+    }
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
     }
 
 }
