6e0a8baef4854bf50509d778fea4813ea5a2306d
Diog007 <diogo.mz123@gmail.com>
2024-02-25 16:29:22 -0300

    Created repository user interface

---
+++ src/main/java/com/diogo/barbernet/api/domain/usuario/UsuarioRepository.java
@@ -0,0 +1,6 @@
+package com.diogo.barbernet.api.domain.usuario;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+}
