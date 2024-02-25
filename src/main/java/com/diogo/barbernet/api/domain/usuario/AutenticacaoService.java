b86948e2acafd8256c8763c0306ec64615689261
Diog007 <diogo.mz123@gmail.com>
2024-02-25 16:33:08 -0300

    Created method to check database user

---
+++ src/main/java/com/diogo/barbernet/api/domain/usuario/AutenticacaoService.java
@@ -0,0 +1,16 @@
+package com.diogo.barbernet.api.domain.usuario;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class AutenticacaoService implements UserDetailsService {
+    @Autowired
+    private UsuarioRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return repository.findByLogin(username);
+    }
+}
--- src/main/java/com/diogo/barbernet/api/domain/usuario/UsuarioRepository.java
+++ src/main/java/com/diogo/barbernet/api/domain/usuario/UsuarioRepository.java
@@ -1,6 +1,8 @@
 package com.diogo.barbernet.api.domain.usuario;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    UserDetails findByLogin(String username);
 }
