f0235dbc58a8127aba865140c711d68d6819e0c3
Diog007 <diogo.mz123@gmail.com>
2024-02-26 21:23:57 -0300

    Created method to validate JWT token

--- src/main/java/com/diogo/barbernet/api/infra/security/TokenService.java
+++ src/main/java/com/diogo/barbernet/api/infra/security/TokenService.java
@@ -3,6 +3,7 @@ package com.diogo.barbernet.api.infra.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.diogo.barbernet.api.domain.usuario.Usuario;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -29,6 +30,20 @@ public class TokenService {
             throw new RuntimeException("Erro ao gerar Token jwt", exception);
         }
     }
+
+    public String validarToken(String tokenJWT){
+        try{
+            var algoritmo = Algorithm.HMAC256(secret);
+            return JWT.require(algoritmo)
+                    .withIssuer("API barbernet")
+                    .build()
+                    .verify(tokenJWT)
+                    .getSubject();
+        }catch (JWTVerificationException exceptione){
+            throw new RuntimeException("Token JWT invalido ou expirado!");
+        }
+    }
+
 
     private Instant dataExpiracao(){
         return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
