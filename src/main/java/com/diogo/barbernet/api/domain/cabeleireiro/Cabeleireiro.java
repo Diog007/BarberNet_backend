5a51f33228e8d4f54501eb2fa2008fc7c75098ff
Diog007 <diogo.mz123@gmail.com>
2024-05-31 17:54:46 -0300

    Adjusted entity relationship

--- src/main/java/com/diogo/barbernet/api/domain/cabeleireiro/Cabeleireiro.java
+++ src/main/java/com/diogo/barbernet/api/domain/cabeleireiro/Cabeleireiro.java
@@ -1,10 +1,12 @@
 package com.diogo.barbernet.api.domain.cabeleireiro;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.diogo.barbernet.api.domain.agendamento.Agendamento;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
 import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Setter
@@ -22,10 +24,22 @@ public class Cabeleireiro {
     private String email;
     private String cpf;
 
+    @JsonIgnore
+    @OneToMany(mappedBy = "cabeleireiro")
+    private List<Agendamento> agendamentos = new ArrayList<>();
+
     public Cabeleireiro(DadosCadastroCabeleireiro dados) {
         this.nome = dados.nome();
         this.telefone = dados.telefone();
         this.email = dados.email();
         this.cpf = dados.cpf();
+    }
+
+    public Cabeleireiro(Long id, String nome, String telefone, String email, String cpf) {
+        this.id = id;
+        this.nome = nome;
+        this.telefone = telefone;
+        this.email = email;
+        this.cpf = cpf;
     }
 }
