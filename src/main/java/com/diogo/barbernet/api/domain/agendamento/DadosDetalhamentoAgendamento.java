09b132410449cbe752a070833fd015e0d85d785d
Diog007 <diogo.mz123@gmail.com>
2024-06-03 15:03:49 -0300

    Adjusted date format

--- src/main/java/com/diogo/barbernet/api/domain/agendamento/DadosDetalhamentoAgendamento.java
+++ src/main/java/com/diogo/barbernet/api/domain/agendamento/DadosDetalhamentoAgendamento.java
@@ -1,13 +1,16 @@
 package com.diogo.barbernet.api.domain.agendamento;
 
-import java.time.LocalDateTime;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
-public record DadosDetalhamentoAgendamento(Long id, Long idCabeleireiro, Long idCliente, LocalDateTime data) {
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public record DadosDetalhamentoAgendamento(Long id, String nomeCabeleireiro, String nomeCliente, @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy HH:mm:ss") LocalDateTime data) {
 
     public DadosDetalhamentoAgendamento(Agendamento agendamento){
         this(   agendamento.getId(),
-                agendamento.getCabeleireiro().getId(),
-                agendamento.getCliente().getId(),
-                agendamento.getDataHora() );
+                agendamento.getCabeleireiro().getNome(),
+                agendamento.getCliente().getNome(),
+                agendamento.getDataHora());
     }
 }
