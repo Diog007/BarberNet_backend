5ca72f82db28364bfb2c5e4208a3f0c308735a0b
Diog007 <diogo.mz123@gmail.com>
2024-06-11 12:32:21 -0300

    Point repository created

---
+++ src/main/java/com/diogo/barbernet/api/domain/ponto/PontoRepository.java
@@ -0,0 +1,6 @@
+package com.diogo.barbernet.api.domain.ponto;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PontoRepository extends JpaRepository<Long, Ponto> {
+}
