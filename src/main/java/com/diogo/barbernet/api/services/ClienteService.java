c0a5d13eb2576f9d906ea47367a049c90f35605f
Diog007 <diogo.mz123@gmail.com>
2024-06-15 14:38:15 -0300

    Implemented Customer email service

--- src/main/java/com/diogo/barbernet/api/services/ClienteService.java
+++ src/main/java/com/diogo/barbernet/api/services/ClienteService.java
@@ -1,10 +1,12 @@
 package com.diogo.barbernet.api.services;
 
+import com.diogo.barbernet.api.controller.EmailController;
 import com.diogo.barbernet.api.domain.ValidacaoException;
 import com.diogo.barbernet.api.domain.cliente.Cliente;
 import com.diogo.barbernet.api.domain.cliente.ClienteRepository;
 import com.diogo.barbernet.api.domain.cliente.DadosAtulizacaoCliente;
 import com.diogo.barbernet.api.domain.cliente.DadosCadastroCliente;
+import com.diogo.barbernet.api.domain.email.EmailDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +19,9 @@ public class ClienteService {
     @Autowired
     private ClienteRepository repository;
 
+    @Autowired
+    private EmailController emailController;
+
     public Cliente findById(Long id) {
         Optional<Cliente> optionalCliente = repository.findById(id);
         return optionalCliente.orElse(null);
@@ -24,6 +29,7 @@ public class ClienteService {
 
     public Cliente cadastrarCliente(DadosCadastroCliente dados){
         Cliente newDados = new Cliente(dados);
+        sendEmailCliente(newDados);
         return repository.save(newDados);
     }
 
@@ -48,4 +54,24 @@ public class ClienteService {
         }
         repository.deleteById(id);
     }
+
+    private void sendEmailCliente(Cliente cliente) {
+        String assunto = "Bem-vindo(a) à Barbernet!";
+        String mensagem = String.format(
+                "Olá, %s,\n\n" +
+                        "Seja bem-vindo(a) à nossa barbearia! Estamos muito contentes em tê-lo(a) conosco.\n" +
+                        "Aqui estão alguns detalhes do seu cadastro:\n\n" +
+                        "Nome: %s\n" +
+                        "E-mail: %s\n" +
+                        "Telefone: %s\n\n" +
+                        "Se precisar de qualquer coisa, não hesite em nos contatar.\n\n" +
+                        "Atenciosamente,\n" +
+                        "Equipe Barbernet",
+                cliente.getNome(), cliente.getNome(), cliente.getEmail(), cliente.getTelefone()
+        );
+
+        EmailDTO email = new EmailDTO("barbernet.api@gmail.com", cliente.getEmail(), assunto, mensagem);
+        this.emailController.sendingEmail(email);
+    }
+
 }
