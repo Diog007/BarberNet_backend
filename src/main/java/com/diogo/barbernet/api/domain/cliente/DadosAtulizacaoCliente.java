fdafe26665d498e2e614f9ec3434d4630c0f0eef
Diog007 <diogo.mz123@gmail.com>
2024-06-17 12:09:02 -0300

    Beans validations for client registration

--- src/main/java/com/diogo/barbernet/api/domain/cliente/DadosAtulizacaoCliente.java
+++ src/main/java/com/diogo/barbernet/api/domain/cliente/DadosAtulizacaoCliente.java
@@ -1,6 +1,21 @@
 package com.diogo.barbernet.api.domain.cliente;
 
-public record DadosAtulizacaoCliente(Long id, String nome, String telefone, String email, String cpf) {
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import org.hibernate.validator.constraints.br.CPF;
+
+public record DadosAtulizacaoCliente(Long id,
+                                     @NotBlank(message = "O nome não pode estar em branco")
+                                     String nome,
+                                     @NotBlank
+                                     String telefone,
+                                     @NotBlank
+                                     @Email(message = "Formato de email inálido!")
+                                     String email,
+                                     @NotBlank
+                                     @CPF(message = "CPF inválido")
+                                     String cpf
+) {
 
 
     public DadosAtulizacaoCliente(Cliente cliente) {
--- src/main/java/com/diogo/barbernet/api/domain/cliente/DadosCadastroCliente.java
+++ src/main/java/com/diogo/barbernet/api/domain/cliente/DadosCadastroCliente.java
@@ -1,6 +1,15 @@
 package com.diogo.barbernet.api.domain.cliente;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
-public record DadosCadastroCliente(String nome, String telefone, String email, String cpf) {
+public record DadosCadastroCliente(
+        @NotBlank(message = "O nome não pode estar em branco")
+        String nome,
+        @NotBlank
+        String telefone,
+        @NotBlank
+        @Email(message = "Formato de email inálido!")
+        String email,
+        String cpf) {
 }
